7211183c077f4cbebe45df3a7afabe87ab9821ad
Luís Felipe <luis.felipe.ferreira.da.silva@hotmail.com>
2025-09-04 09:02:32 -0300

    Adicionando o Model Produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    estoque = models.IntegerField(default=0)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
